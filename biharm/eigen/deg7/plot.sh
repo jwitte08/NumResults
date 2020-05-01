aa287698a2152cf861c8bbd787682e51004da6fd
Julius Witte <julius.witte@iwr.uni-heidelberg.de>
2020-05-01 12:59:51 +0200

    1D eigendecompositions for biharmonic problem
    vertex patch
    nodal elements in GLL points

---
+++ biharm/eigen/deg7/plot.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+python3 ../../scripts/plot_biharmeigenfunctions.py -N 13 -grd "4x4" \
+	-spc 'Bip_wrt_sqL,Eigenfunctions of $B_{ip} v_i = \lambda_i L^2 v_i$' \
+	'L_wrt_M,Eigenfunctions of $L v_i = \lambda_i M v_i$' \
+	'Bip_wrt_M,Eigenfunctions of $B_{ip} v_i = \lambda_i M v_i$' \
+	'Bip,Eigenfunctions of $B_{ip} v_i = \lambda_i v_i$' \
+	'B,Eigenfunctions of $B v_i = \lambda_i v_i$' \
+	'L,Eigenfunctions of $L v_i = \lambda_i v_i$' \
+	'sqL,Eigenfunctions of $L^2 v_i = \lambda_i v_i$' \
+	'M,Eigenfunctions of $M v_i = \lambda_i v_i$' \
